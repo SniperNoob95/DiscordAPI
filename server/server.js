4e0d5bdd67efe639c5c68fc812a8edfe15f29e47
justinm1295 <myersonj@gmail.com>
2020-03-19 18:53:11 -0400

    Added body parser.

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const apiRouter = require('./routes');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const port = 80;
 
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api', apiRouter);
 
